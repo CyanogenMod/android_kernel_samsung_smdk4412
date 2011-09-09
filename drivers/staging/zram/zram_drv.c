c3fb096d4a4de7eedaafcce4b5c7c387467913a2
Nitin Gupta <ngupta@vflare.org>
2011-09-09 19:01:00 -0400

    zram: Fix sparse warnings
    Fixes sparse warning:
    zram_drv.c:666:6: warning: symbol 'zram_slot_free_notify' was not
    declared. Should it be static?
    
    Also, max_zpage_size is now size_t just to be consistent with data-type
    of other variables maintaining sizes of various kinds.
    
    Signed-off-by: Nitin Gupta <ngupta@vflare.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -570,7 +570,8 @@ fail:
 	return ret;
 }
 
-void zram_slot_free_notify(struct block_device *bdev, unsigned long index)
+static void zram_slot_free_notify(struct block_device *bdev,
+				unsigned long index)
 {
 	struct zram *zram;
 
