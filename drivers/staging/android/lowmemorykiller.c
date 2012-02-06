e6edd4723129b31402ad2da3d579abfaf5286f4d
Anton Vorontsov <anton.vorontsov@linaro.org>
2012-02-06 20:29:41 +0400

    staging: android/lowmemorykiller: Don't grab tasklist_lock
    Grabbing tasklist_lock has its disadvantages, i.e. it blocks
    process creation and destruction. If there are lots of processes,
    blocking doesn't sound as a great idea.
    
    For LMK, it is sufficient to surround tasks list traverse with
    rcu_read_{,un}lock().
    
    >From now on using force_sig() is not safe, as it can race with an
    already exiting task, so we use send_sig() now. As a downside, it
    won't kill PID namespace init processes, but that's not what we
    want anyway.
    
    Suggested-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Anton Vorontsov <anton.vorontsov@linaro.org>
    Reviewed-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	drivers/staging/android/lowmemorykiller.c

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -34,6 +34,7 @@
 #include <linux/mm.h>
 #include <linux/oom.h>
 #include <linux/sched.h>
+#include <linux/rcupdate.h>
 #include <linux/notifier.h>
 #define ENHANCED_LMK_ROUTINE
 
@@ -178,7 +179,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	selected_oom_adj = min_adj;
 #endif
 
-	read_lock(&tasklist_lock);
+	rcu_read_lock();
 	for_each_process(p) {
 		struct mm_struct *mm;
 		struct signal_struct *sig;
@@ -273,13 +274,13 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			     selected_oom_adj, selected_tasksize);
 		lowmem_deathpending = selected;
 		lowmem_deathpending_timeout = jiffies + HZ;
-		force_sig(SIGKILL, selected);
+		send_sig(SIGKILL, selected, 0);
 		rem -= selected_tasksize;
 	}
 #endif
 	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
-	read_unlock(&tasklist_lock);
+	rcu_read_unlock();
 	return rem;
 }
 
