5eb00c0353bc1987b1e40759c172dd2a839a4707
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2012-07-22 23:34:38 +0200

    fix adb root
    When adb is enabled, only connect the gadget when adbd is ready. If adbd
    dies or is restarted (e.g. "adb root"), the gadget is disconnected when
    the adb device is close, and it is re-connected once adb re-open the
    device.
    
    - Add callbacks to adb, similar to FunctionFs callbacks, to notify the
      gadget when the daemon is ready or closed.
    - Refcount calls to android_enable/android_disable to enable the gadget
      only once all the function daemons are ready.
    - Add enable/disble to android_usb_function to notify the function when
      it is added/removed from the list of enabled functions.
    
    Signed-off-by: Benoit Goby <benoit@android.com>

--- drivers/usb/gadget/f_adb.c
+++ drivers/usb/gadget/f_adb.c
@@ -57,6 +57,9 @@ struct adb_dev {
 	int rx_done;
 };
 
+static void adb_ready_callback(void);
+static void adb_closed_callback(void);
+
 static struct usb_interface_descriptor adb_interface_desc = {
 	.bLength                = USB_DT_INTERFACE_SIZE,
 	.bDescriptorType        = USB_DT_INTERFACE,
@@ -441,7 +444,7 @@ static ssize_t adb_write(struct file *fp, const char __user *buf,
 
 static int adb_open(struct inode *ip, struct file *fp)
 {
-	printk(KERN_INFO "adb_open\n");
+	pr_info("adb_open\n");
 	if (!_adb_dev)
 		return -ENODEV;
 
@@ -453,12 +456,15 @@ static int adb_open(struct inode *ip, struct file *fp)
 	/* clear the error latch */
 	_adb_dev->error = 0;
 
+    adb_ready_callback();
+
 	return 0;
 }
 
 static int adb_release(struct inode *ip, struct file *fp)
 {
-	printk(KERN_INFO "adb_release\n");
+	pr_info("adb_release\n");
+    adb_closed_callback();
 	adb_unlock(&_adb_dev->open_excl);
 	return 0;
 }
@@ -477,9 +483,6 @@ static struct miscdevice adb_device = {
 	.name = adb_shortname,
 	.fops = &adb_fops,
 };
-
-
-
 
 static int
 adb_function_bind(struct usb_configuration *c, struct usb_function *f)
