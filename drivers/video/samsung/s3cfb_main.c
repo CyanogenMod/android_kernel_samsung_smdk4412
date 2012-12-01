24b928e39a99f4f69d6c7008c7887509b910d0ce
Andrew Dodd <atd7@cornell.edu>
2012-12-01 10:50:03 +0100

    s3cfb: Notify userspace of vsync using sysfs, make uevents optional
    Change-Id: I6fe8de47961d5c0ba73eb45543a675b6fd4344a4

--- drivers/video/samsung/s3cfb_main.c
+++ drivers/video/samsung/s3cfb_main.c
@@ -387,6 +387,18 @@ static ssize_t fimd_dump_show(struct device *dev,
 }
 static DEVICE_ATTR(fimd_dump, 0444, fimd_dump_show, NULL);
 
+#ifdef CONFIG_FB_S5P_VSYNC_SYSFS
+static ssize_t s3c_fb_vsync_time(struct device *dev,
+                struct device_attribute *attr, char *buf)
+{
+    struct s3cfb_global *fbdev = fbfimd->fbdev[0];
+
+    return snprintf(buf, PAGE_SIZE, "%llu", ktime_to_ns(fbdev->vsync_info.timestamp));
+}
+
+static DEVICE_ATTR(vsync_time, S_IRUGO, s3c_fb_vsync_time, NULL);
+#endif
+
 #if 0 /* def CONFIG_FB_S5P_MIPI_DSIM */
 void s3cfb_display_on_remote(void)
 {
@@ -430,6 +442,7 @@ static int s3cfb_wait_for_vsync_thread(void *data)
 						msecs_to_jiffies(VSYNC_TIMEOUT_MSEC));
 
 		if (ret > 0) {
+#if defined(CONFIG_FB_S5P_VSYNC_SEND_UEVENTS)
 			char *envp[2];
 			char buf[64];
 			snprintf(buf, sizeof(buf), "VSYNC=%llu",
@@ -438,6 +451,10 @@ static int s3cfb_wait_for_vsync_thread(void *data)
 			envp[1] = NULL;
 			kobject_uevent_env(&fbdev->dev->kobj, KOBJ_CHANGE,
 							envp);
+#endif
+#if defined(CONFIG_FB_S5P_VSYNC_SYSFS)
+            sysfs_notify(&fbdev->dev->kobj, NULL, "vsync_time");
+#endif
 		}
 	}
 
@@ -666,6 +683,12 @@ static int s3cfb_probe(struct platform_device *pdev)
 		ret = device_create_file(fbdev[i]->dev, &dev_attr_fimd_dump);
 		if (ret < 0)
 			dev_err(fbdev[0]->dev, "failed to add sysfs entries\n");
+
+#ifdef CONFIG_FB_S5P_VSYNC_SYSFS
+        ret = device_create_file(fbdev[i]->dev, &dev_attr_vsync_time);
+        if (ret < 0)
+            dev_err(fbdev[0]->dev, "failed to add sysfs entries\n");
+#endif
 
 		ret = device_create_file(fbdev[i]->dev, &dev_attr_ielcd_dump);
 		if (ret < 0)
