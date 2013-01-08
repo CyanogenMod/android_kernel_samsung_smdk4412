c8885ec7cd8b5728535f81c84fb721078b063aa9
Espen Fjellvær Olsen <espen@mrfjo.org>
2013-01-08 20:53:54 +0100

    Revert "exynos-mem: Add fimc1 to mmap whitelist check"
    This reverts commit eaa468eae8c9d87e4c83b30d977e744b3e373613.

--- drivers/char/exynos_mem.c
+++ drivers/char/exynos_mem.c
@@ -266,6 +266,12 @@ void cma_region_descriptor_add(const char *name, int start, int size)
 
 int exynos_mem_mmap(struct file *filp, struct vm_area_struct *vma)
 {
+
+/* Devices not having DMA CMA acess shouldn't be using this in any case at all */
+#ifndef CONFIG_DMA_CMA
+	return -EINVAL;
+#endif
+
 	struct exynos_mem *mem = (struct exynos_mem *)filp->private_data;
 	bool cacheable = mem->cacheable;
 	dma_addr_t start = 0;
@@ -303,8 +309,8 @@ int exynos_mem_mmap(struct file *filp, struct vm_area_struct *vma)
 			 *
 			 * Add exceptions as we go.
 			 */
-			if(strcmp(b->name, "s3c-fimc") == 0 ||
-			   strcmp(b->name, "fimc1") == 0) {
+
+			if(strcmp(b->name, "s3c-fimc") == 0) {
 				allowed = true;
 				pr_info("[%s] Accessing space 0x%08x/0x%08x for '%s'\n",
 					__func__, b->start, b->size, b->name);
