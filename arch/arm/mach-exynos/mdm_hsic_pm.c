85ce5b72365c2357c818365a724fe8bf572968d7
codeworkx <codeworkx@cyanogenmod.org>
2013-01-12 18:58:20 +0000

    t0lte: support for galaxy note 2 lte
    Change-Id: I9ac93ae5b0746c4b8af0a430180b309638935c49

--- arch/arm/mach-exynos/mdm_hsic_pm.c
+++ arch/arm/mach-exynos/mdm_hsic_pm.c
@@ -36,10 +36,12 @@
 #include <linux/usb/hcd.h>
 #include <linux/usb/ehci_def.h>
 
+#ifdef CONFIG_CPU_FREQ_TETHERING
 #include <linux/kernel.h>
 #include <linux/netdevice.h>
 #include <mach/mdm2.h>
 #include <linux/usb/android_composite.h>
+#endif
 
 #define EXTERNAL_MODEM "external_modem"
 #define EHCI_REG_DUMP
@@ -85,8 +87,10 @@ struct mdm_hsic_pm_data {
 
 	/* control variables */
 	struct notifier_block pm_notifier;
+#ifdef CONFIG_CPU_FREQ_TETHERING
 	struct notifier_block netdev_notifier;
 	struct notifier_block usb_composite_notifier;
+#endif
 
 	bool block_request;
 	bool state_busy;
@@ -233,6 +237,49 @@ static struct mdm_hsic_pm_data *get_pm_data_by_dev_name(const char *name)
 	return NULL;
 }
 
+/* do not call in irq context */
+int pm_dev_runtime_get_enabled(struct usb_device *udev)
+{
+	int spin = 50;
+
+	while (spin--) {
+		pr_debug("%s: rpm status: %d\n", __func__,
+						udev->dev.power.runtime_status);
+		if (udev->dev.power.runtime_status == RPM_ACTIVE ||
+			udev->dev.power.runtime_status == RPM_SUSPENDED) {
+			usb_mark_last_busy(udev);
+			break;
+		}
+		msleep(20);
+	}
+	if (spin <= 0) {
+		pr_err("%s: rpm status %d, return -EAGAIN\n", __func__,
+						udev->dev.power.runtime_status);
+		return -EAGAIN;
+	}
+	usb_mark_last_busy(udev);
+
+	return 0;
+}
+
+/* do not call in irq context */
+int pm_dev_wait_lpa_wake(void)
+{
+	int spin = 50;
+
+	while (lpa_handling && spin--) {
+		pr_debug("%s: lpa wake wait loop\n", __func__);
+		msleep(20);
+	}
+
+	if (lpa_handling) {
+		pr_err("%s: in lpa wakeup, return EAGAIN\n", __func__);
+		return -EAGAIN;
+	}
+
+	return 0;
+}
+
 void notify_modem_fatal(void)
 {
 	struct mdm_hsic_pm_data *pm_data =
@@ -253,6 +300,8 @@ void notify_modem_fatal(void)
 
 		pm_runtime_get_noresume(dev);
 		pm_runtime_dont_use_autosuspend(dev);
+		/* if it's in going suspend, give settle time before wake up */
+		msleep(100);
 		wake_up_all(&dev->power.wait_queue);
 		pm_runtime_resume(dev);
 		pm_runtime_get_noresume(dev);
@@ -825,6 +874,11 @@ static int mdm_hsic_pm_gpio_init(struct mdm_hsic_pm_data *pm_data,
 		if (ret < 0)
 			return ret;
 		gpio_direction_output(pm_data->gpio_host_ready, 1);
+		s3c_gpio_cfgpin(pm_data->gpio_host_ready, S3C_GPIO_OUTPUT);
+		s3c_gpio_setpull(pm_data->gpio_host_ready, S3C_GPIO_PULL_NONE);
+		s5p_gpio_set_drvstr(pm_data->gpio_host_ready,
+							S5P_GPIO_DRVSTR_LV4);
+		gpio_set_value(pm_data->gpio_host_ready, 1);
 	} else
 		return -ENXIO;
 
@@ -879,6 +933,7 @@ static void mdm_hsic_pm_gpio_free(struct mdm_hsic_pm_data *pm_data)
 		gpio_free(pm_data->gpio_host_wake);
 }
 
+#ifdef CONFIG_CPU_FREQ_TETHERING
 static int link_pm_netdev_event(struct notifier_block *this,
 				unsigned long event, void *ptr)
 {
@@ -932,6 +987,7 @@ static int usb_composite_notifier_event(struct notifier_block *this,
 
 	return NOTIFY_DONE;
 }
+#endif
 
 static int mdm_hsic_pm_probe(struct platform_device *pdev)
 {
@@ -992,12 +1048,14 @@ static int mdm_hsic_pm_probe(struct platform_device *pdev)
 	blocking_notifier_chain_register(&mdm_reset_notifier_list,
 							&mdm_reset_main_block);
 
+#ifdef CONFIG_CPU_FREQ_TETHERING
 	pm_data->netdev_notifier.notifier_call = link_pm_netdev_event;
 	register_netdevice_notifier(&pm_data->netdev_notifier);
 
 	pm_data->usb_composite_notifier.notifier_call =
 		usb_composite_notifier_event;
 	register_usb_composite_notifier(&pm_data->usb_composite_notifier);
+#endif
 
 	wake_lock_init(&pm_data->l2_wake, WAKE_LOCK_SUSPEND, pm_data->name);
 	wake_lock_init(&pm_data->boot_wake, WAKE_LOCK_SUSPEND, "mdm_boot");
