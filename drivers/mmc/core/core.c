1f5e693472397bb780e04b12218bb49a4c4d6230
andip71 <andreasp@gmx.de>
2013-01-15 09:07:58 +0100

    mmc-core: Increase timeout limits to improve when using bad sd cards
    credits to AndreiLux
    
    Change-Id: If3a2fb9215ffdce9b3ab525648732dd493a3bd53

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -577,9 +577,9 @@ void mmc_set_data_timeout(struct mmc_data *data, const struct mmc_card *card)
 			 * The limit is really 250 ms, but that is
 			 * insufficient for some crappy cards.
 			 */
-			limit_us = 300000;
+			limit_us = 1500000;
 		else
-			limit_us = 100000;
+			limit_us = 500000;
 
 		/*
 		 * SDHC cards always use these fixed values.
