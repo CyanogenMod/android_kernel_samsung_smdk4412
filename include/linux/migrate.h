ab6dfccd8d126b50059d39d031cfa1ddc8f32b84
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-05-25 10:48:35 +0200

    migrate: fix merge fail
    Change-Id: Ic7caec90cd2c7bb977040d4e416016ca36ee4eea

--- include/linux/migrate.h
+++ include/linux/migrate.h
@@ -60,20 +60,16 @@ static inline void putback_lru_pages(struct list_head *l) {}
 #ifndef CONFIG_DMA_CMA
 static inline int migrate_pages(struct list_head *l, new_page_t x,
 		unsigned long private, bool offlining,
-<<<<<<< HEAD
-		bool sync) { return -ENOSYS; }
+		enum migrate_mode mode) { return -ENOSYS; }
 #else
 static inline int migrate_pages(struct list_head *l, new_page_t x,
 		unsigned long private, bool offlining,
-		bool sync, int tries) { return -ENOSYS; }
+		enum migrate_mode mode, int tries) { return -ENOSYS; }
 
 static inline int migrate_replace_cma_page(struct page *oldpage,
 		struct page **newpage) { return -ENOSYS; }
 #endif
 
-=======
-		enum migrate_mode mode) { return -ENOSYS; }
->>>>>>> 54ea5b40f067cf098cac639973c6628c6944cfb2
 static inline int migrate_huge_pages(struct list_head *l, new_page_t x,
 		unsigned long private, bool offlining,
 		enum migrate_mode mode) { return -ENOSYS; }
