e0d43667b66bec7c2b59973ce3203f97af7688dc
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-08-19 22:03:11 +0200

    n7100: add cyanogenmod mdnie preset
    Change-Id: I42e3ec88452239a6b953dff0e636a8c2e5dbf958

--- drivers/video/samsung/mdnie_table_t0.h
+++ drivers/video/samsung/mdnie_table_t0.h
@@ -3,6 +3,18 @@
 
 #include "mdnie.h"
 
+static const unsigned short tune_cyanogenmod[] = {
+	0x0000, 0x0000,	/*BANK 0*/
+	0x0008, 0x0008,	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0030, 0x0000,	/*FA cs1 de8 hdr2 fa1*/
+	0x0090, 0x0080,	/*DE egth*/
+	0x00b0, 0x1010,	/*CS hg ry*/
+	0x00b1, 0x1010,	/*CS hg gc*/
+	0x00b2, 0x1010,	/*CS hg bm*/
+	0x00b3, 0x1204,	/*CS weight grayTH*/
+	0x00ff, 0x0000,	/*Mask Release*/
+	END_SEQ, 0x0000,
+};
 
 static const unsigned short tune_dynamic_gallery[] = {
 	0x0000, 0x0000,	/*BANK 0*/
@@ -638,6 +650,7 @@ struct mdnie_tunning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
 struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 	{
 		{
+			{"CYANOGENMOD",		tune_cyanogenmod},
 			{"DYNAMIC_UI",		tune_dynamic_ui},
 			{"DYNAMIC_VIDEO",	tune_dynamic_video},
 			{"DYNAMIC_VIDEO",	tune_dynamic_video},
@@ -647,6 +660,7 @@ struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 			{"DYNAMIC_GALLERY",	tune_dynamic_gallery},
 			{"DYNAMIC_VT",		tune_dynamic_vt},
 		}, {
+			{"CYANOGENMOD",		tune_cyanogenmod},
 			{"STANDARD_UI",		tune_standard_ui},
 			{"STANDARD_VIDEO",	tune_standard_video},
 			{"STANDARD_VIDEO",	tune_standard_video},
@@ -656,6 +670,7 @@ struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 			{"STANDARD_GALLERY",	tune_standard_gallery},
 			{"STANDARD_VT",		tune_standard_vt},
 		}, {
+			{"CYANOGENMOD",		tune_cyanogenmod},
 			{"NATURAL_UI",		tune_natural_ui},
 			{"NATURAL_VIDEO",	tune_natural_video},
 			{"NATURAL_VIDEO_WARM",	tune_natural_video},
@@ -665,6 +680,7 @@ struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 			{"NATURAL_GALLERY",	tune_natural_gallery},
 			{"NATURAL_VT",		tune_natural_vt},
 		}, {
+			{"CYANOGENMOD",		tune_cyanogenmod},
 			{"MOVIE_UI",		tune_movie_ui},
 			{"MOVIE_VIDEO",		tune_movie_video},
 			{"MOVIE_VIDEO",		tune_movie_video},
