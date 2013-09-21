d5b001b42ba995e3015423539d00add44d79a509
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-09-21 22:51:39 +0200

    Revert "wakelock: use single threaded workqueueus"
    This reverts commit 7cf296f8064db7991c4cffb13e07de1b5a981d4a.
    
    Change-Id: Iff69627417b6607e9384019154c15b0ae0e8a20b

--- kernel/power/wakelock.c
+++ kernel/power/wakelock.c
@@ -630,7 +630,7 @@ static int __init wakelocks_init(void)
 		goto err_platform_driver_register;
 	}
 
-	suspend_work_queue = alloc_workqueue("suspend", WQ_UNBOUND|WQ_HIGHPRI, 0);
+	suspend_work_queue = alloc_workqueue("suspend", WQ_HIGHPRI, 0);
 	if (suspend_work_queue == NULL) {
 		ret = -ENOMEM;
 		goto err_suspend_work_queue;
