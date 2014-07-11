a784c375212d90c5ae7b75b8ccc67c6e0d364acd
sbrissen <sbrissen@hotmail.com>
2014-07-11 14:07:27 -0400

    smdk4412: fix kona bootloop
    Change-Id: I21c2828a547f8387dada2e6393d92fe115e2491f

--- drivers/gpu/mali400/r3p2/mali/common/mali_mem_validation.c
+++ drivers/gpu/mali400/r3p2/mali/common/mali_mem_validation.c
@@ -13,7 +13,9 @@
 #include "mali_kernel_common.h"
 /* MALI_SEC */
 #if defined(CONFIG_CPU_EXYNOS4212) || defined(CONFIG_CPU_EXYNOS4412)
+#ifndef CONFIG_MACH_KONA
 #define MALI_SEC_MEM_VALIDATION
+#endif
 #include <linux/cma.h>
 #include <plat/pd.h>
 #include <linux/platform_device.h>
