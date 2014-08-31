351087411d2bd7d0acf77e014092f2eb1ef00da2
andip71 <andreasp@gmx.de>
2014-08-31 23:54:47 +0200

    tmpfs: fix mempolicy object leaks
    This patch fixes several mempolicy leaks in the tmpfs mount logic.
    These leaks are slow - on the order of one object leaked per mount
    attempt.
    
    Leak 1 (umount doesn't free mpol allocated in mount):
        while true; do
            mount -t tmpfs -o mpol=interleave,size=100M nodev /mnt
            umount /mnt
        done
    
    Leak 2 (errors parsing remount options will leak mpol):
        mount -t tmpfs -o size=100M nodev /mnt
        while true; do
            mount -o remount,mpol=interleave,size=x /mnt 2> /dev/null
        done
        umount /mnt
    
    Leak 3 (multiple mpol per mount leak mpol):
        while true; do
            mount -t tmpfs -o mpol=interleave,mpol=interleave,size=100M nodev /mnt
            umount /mnt
        done
    
    This patch fixes all of the above.  I could have broken the patch into
    three pieces but is seemed easier to review as one.
    
    Signed-off-by: Greg Thelen <gthelen@google.com>
    
    (https://lkml.org/lkml/2013/2/20/55)
    
    Change-Id: Ie08a9cb8d908289b8ecda816a8085c4bca68505d

--- mm/shmem.c
+++ mm/shmem.c
@@ -2412,6 +2412,7 @@ static int shmem_parse_options(char *options, struct shmem_sb_info *sbinfo,
 			       bool remount)
 {
 	char *this_char, *value, *rest;
+	struct mempolicy *mpol = NULL;
 
 	while (options != NULL) {
 		this_char = options;
@@ -2438,7 +2439,7 @@ static int shmem_parse_options(char *options, struct shmem_sb_info *sbinfo,
 			printk(KERN_ERR
 			    "tmpfs: No value for mount option '%s'\n",
 			    this_char);
-			return 1;
+			goto error;
 		}
 
 		if (!strcmp(this_char,"size")) {
@@ -2481,19 +2482,25 @@ static int shmem_parse_options(char *options, struct shmem_sb_info *sbinfo,
 			if (*rest)
 				goto bad_val;
 		} else if (!strcmp(this_char,"mpol")) {
-			if (mpol_parse_str(value, &sbinfo->mpol, 1))
+			mpol_put(mpol);
+			if (mpol_parse_str(value, &mpol, 1)) {
+				mpol = NULL;
 				goto bad_val;
+			}
 		} else {
 			printk(KERN_ERR "tmpfs: Bad mount option %s\n",
 			       this_char);
-			return 1;
+			goto error;
 		}
 	}
+	sbinfo->mpol = mpol;
 	return 0;
 
 bad_val:
 	printk(KERN_ERR "tmpfs: Bad value '%s' for mount option '%s'\n",
 	       value, this_char);
+error:
+	mpol_put(mpol);
 	return 1;
 
 }
@@ -2568,6 +2575,7 @@ static void shmem_put_super(struct super_block *sb)
 	struct shmem_sb_info *sbinfo = SHMEM_SB(sb);
 
 	percpu_counter_destroy(&sbinfo->used_blocks);
+	mpol_put(sbinfo->mpol);
 	kfree(sbinfo);
 	sb->s_fs_info = NULL;
 }
