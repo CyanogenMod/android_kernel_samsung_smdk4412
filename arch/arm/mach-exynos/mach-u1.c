40eeddfb1d85b7741e6bce1bb79048fc10dd16be
173210 <root.3.173210@live.com>
2015-04-13 19:52:34 +0900

    mach-u1.c: Fix a compile bug
    Change-Id: I7832308a6381c6d23365751b93960dfafebb268c

--- arch/arm/mach-exynos/mach-u1.c
+++ arch/arm/mach-exynos/mach-u1.c
@@ -4604,9 +4604,7 @@ static struct sec_bat_adc_table_data temper_table_ADC7[] =  {
 	{ 1669,	 -60 },
 	{ 1688,	 -70 },
 };
-#endif
-/* temperature table for ADC 7 */
-#ifdef CONFIG_TARGET_LOCALE_NA
+#elif defined(CONFIG_TARGET_LOCALE_NA)
 static struct sec_bat_adc_table_data  temper_table_ADC7[] =  {
 	{  145,  670 },
 	{  165,  660 },
