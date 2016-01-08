9d2e392cc54a3504b378dde116195ee0273a917b
Ziyan <jaraidaniel@gmail.com>
2016-01-08 12:29:44 +0100

    zram: default to LZ4 compression if LZ4 is enabled
    Change-Id: I4ac454ceb60f960c30c9719d0aa63e4bda7dfbd5

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -40,7 +40,11 @@
 /* Globals */
 static int zram_major;
 static struct zram *zram_devices;
+#ifdef CONFIG_ZRAM_LZ4_COMPRESS
+static const char *default_compressor = "lz4";
+#else
 static const char *default_compressor = "lzo";
+#endif
 
 /*
  * We don't need to see memory allocation errors more than once every 1
