8cb931ff83f1b6eaaf4b2a2c27a57a330ed2489a
Simon Shields <keepcalm444@gmail.com>
2016-03-02 22:14:55 +1100

    cpufreq: pegasusq: boost: drop more debugging
    Change-Id: I607c3a8a70857bad2d3f13d666f987ae2c22329f

--- drivers/cpufreq/cpufreq_pegasusq.c
+++ drivers/cpufreq/cpufreq_pegasusq.c
@@ -900,7 +900,6 @@ static ssize_t store_boost_lock_time(struct kobject *a, struct attribute *b,
 			time = ktime_set(0, (unsigned long)input);
 			hrtimer_init(&boost_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 			boost_timer.function = &end_boost;
-			printk(KERN_DEBUG "[boost] starting boost lock: %lu\n", input);
 			hrtimer_start(&boost_timer, time, HRTIMER_MODE_REL);
 		}
 	} else {
