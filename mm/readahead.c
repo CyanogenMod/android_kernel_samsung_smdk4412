ae79b0c5b3890ffefe64f25bced62b91ab58ad44
Trafalgar-Square <schneller.demian@gmail.com>
2016-06-12 14:38:05 +0200

    Revert "readhead: Optimize divide/multiply by power of 2 using L/R shift from ramgear"
    This reverts commit 321374d149f04b8a2dacf426697647bde0b90538.

--- mm/readahead.c
+++ mm/readahead.c
@@ -218,7 +218,7 @@ int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 	while (nr_to_read) {
 		int err;
 
-		unsigned long this_chunk = (2097152) >> PAGE_CACHE_SHIFT;
+		unsigned long this_chunk = (2 * 1024 * 1024) / PAGE_CACHE_SIZE;
 
 		if (this_chunk > nr_to_read)
 			this_chunk = nr_to_read;
@@ -242,7 +242,7 @@ int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 unsigned long max_sane_readahead(unsigned long nr)
 {
 	return min(nr, (node_page_state(numa_node_id(), NR_INACTIVE_FILE)
-		+ node_page_state(numa_node_id(), NR_FREE_PAGES)) >> 1);
+		+ node_page_state(numa_node_id(), NR_FREE_PAGES)) / 2);
 }
 
 /*
@@ -269,10 +269,10 @@ static unsigned long get_init_ra_size(unsigned long size, unsigned long max)
 {
 	unsigned long newsize = roundup_pow_of_two(size);
 
-	if (newsize <= (max >> 5))
-		newsize = newsize << 2;
-	else if (newsize <= (max >> 2))
-		newsize = newsize << 1;
+	if (newsize <= max / 32)
+		newsize = newsize * 4;
+	else if (newsize <= max / 4)
+		newsize = newsize * 2;
 	else
 		newsize = max;
 
@@ -289,10 +289,10 @@ static unsigned long get_next_ra_size(struct file_ra_state *ra,
 	unsigned long cur = ra->size;
 	unsigned long newsize;
 
-	if (cur < (max >> 4))
-		newsize = cur >> 2;
+	if (cur < max / 16)
+		newsize = 4 * cur;
 	else
-		newsize = cur >> 1;
+		newsize = 2 * cur;
 
 	return min(newsize, max);
 }
@@ -380,7 +380,7 @@ static int try_context_readahead(struct address_space *mapping,
 	 * it is a strong indication of long-run stream (or whole-file-read)
 	 */
 	if (size >= offset)
-		size <<= 1;
+		size *= 2;
 
 	ra->start = offset;
 	ra->size = get_init_ra_size(size + req_size, max);
